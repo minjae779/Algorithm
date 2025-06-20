15512661c12ccc3f0199db4d7d6d2a9ce5ea3b5b
201910779 <62335649+minjae779@users.noreply.github.com>
2025-06-20 09:29:14 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.12 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,8 @@
+def solution(array, commands):
+    answer = []
+    for i in commands:
+        x = []
+        x = array[i[0]-1:i[1]]
+        x.sort()
+        answer.append(x[i[2]-1])
+    return answer
